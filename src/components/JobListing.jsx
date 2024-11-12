a39e6979a9fa60b64f7838804a04c9aa31e8fbbc
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-11-12 15:17:17 +0200

    onClick event added to button for showing less or more text

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -21,8 +21,11 @@ const JobListing = ({ job }) => {
 
           <div className="mb-5">{description}</div>
 
-          <button className="text-indigo-500 mb-5 hover:text-indigo-600">
-            { showFullDescription ? 'Less' : 'More'}
+          <button
+            onClick={() => setShowFullDescription((prevState) => !prevState)}
+            className="text-indigo-500 mb-5 hover:text-indigo-600"
+          >
+            {showFullDescription ? "Less" : "More"}
           </button>
 
           <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>

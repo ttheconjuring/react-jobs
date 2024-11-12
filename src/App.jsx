e9cbc0e0822e6dd127ec2ccfc31932f84fa61807
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-11-12 15:58:54 +0200

    MainLayout introduced

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import React from "react";
-import HomePage from "./pages/HomePage"
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
 );
 
 const App = () => {

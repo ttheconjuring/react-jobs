6c6af89fb73d7c4190e02875baad08f892d99ab8
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-11-12 16:07:08 +0200

    D:/download/Git/job route added

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import {
 import React from "react";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
     </Route>
   )
 );

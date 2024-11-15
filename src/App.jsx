ed1d3a979f26b805217cbf84c9ca24039b6fb106
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-11-15 15:34:57 +0200

    new component created - AddJobPage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
       <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+      <Route path="/add-job" element={<AddJobPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
